ae02b956993020d28ab4ef2fd879a6a0112fb07e
Igor de Oliveira Costa <igorcosta@ifbapa.intra>
2023-03-08 10:34:16 -0300

    first commit

---
+++ src/Principal.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		
+		int a = in.nextInt();
+		int b = in.nextInt();
+		int c = a+b;
+		System.out.println("O resultado da soma é: "+ c);
+
+	}
+
+}
