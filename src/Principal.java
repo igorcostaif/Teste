95ebc34c953b3a5af3551274545b3538c2fa8175
Igor de Oliveira Costa <igorcosta@ifbapa.intra>
2023-03-09 12:08:46 -0300

    2 versao

--- src/Principal.java
+++ src/Principal.java
@@ -10,6 +10,8 @@ public class Principal {
 		int a = in.nextInt();
 		int b = in.nextInt();
 		int c = a+b;
+		
+		System.out.println("Teste");
 		System.out.println("O resultado da soma é: "+ c);
 
 	}
